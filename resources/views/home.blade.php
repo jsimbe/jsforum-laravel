b4207c4c4f3397d5d418a81a96d90be83adaf072
J <jsimbej@gmail.com>
2015-11-27 19:51:59 +0800

    fixed user and comment links on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,8 @@ There is no post till now. Login and write a new post!
       <h3>
         <a href="{{ url('/'.$post->slug) }}">{{ $post->title }}</a>
       </h3>
-      <p>{{ $post->created_at->format('M d,Y \a\t h:i a') }} By <a href="{{ url("/user/".$post->author_id) }}">{{ $post->author->name }}</a></p>
-      <h5>{{ count($post->comments) }} comments</h5>
+      <p>{{ $post->created_at->format('M d,Y \a\t h:i a') }} By {{ $post->author->name }}</p>
+      <a href="{{ url('/'.$post->slug) }}"><h5>{{ count($post->comments) }} comments</h5></a>
     </div>
     <div class="list-group-item">
       <article>
