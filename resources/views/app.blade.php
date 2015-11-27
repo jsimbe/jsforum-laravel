839badad75ddd6013f69273193bcc5f99a13c0d8
J <jsimbej@gmail.com>
2015-11-27 18:22:43 +0800

    added meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta title="FAD.LOL">
+  <meta name="description" content="yet another forum">
 	<title>FAD.LOL</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
